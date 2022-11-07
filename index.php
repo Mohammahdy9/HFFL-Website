090bda1dd0ac0e691bfa2cd33f3a2d2435220663
Mohammahdy9 <72105916+Mohammahdy9@users.noreply.github.com>
2022-11-06 20:52:41 -0600

    Newest commits

--- index.php
+++ index.php
@@ -4,29 +4,53 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>HFFL APP</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      border: 1px solid gray;
+      width: calc(50% - 10px);
+      float: left;
+      margin: 5px;
+  }
+  </style>
 </head>
 <body>
   <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
     <img class="logo" src="HFFL images/hffl.jpg" alt="HFFL LOGO" width="85">
-    <nav>
-      <ul class="nav_links">
-        <li><a href="teams.html">Teams</a></li>
-        <li><a href="players.php">Players</a></li>
-        <li><a href="tournaments.html">Tournaments</a></li>
-        <li><a href="matches.html">Matches</a></li>
-      </ul >
-    </nav>
-    <a class="cta" href="#"><button type="button" name="button">Admin</button></a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="players.php">Players</a>
+        <a class="nav-link" href="teams.php">Teams</a>
+        <a class="nav-link" href="matches.php">Matches</a>
+        <a class="nav-link" href="tournaments.php">Tournaments</a>
+        <a href="players.php" class="btn btn-danger" role="button">ADMIN</a>
+      </div>
+    </div>
+  </div>
+  </nav>
   </header>
   <section>
     <script src="https://cdn.logwork.com/widget/countdown.js"></script>
     <a href="https://logwork.com/countdown-wa7c" class="countdown-timer" data-style="circles" data-timezone="America/Chicago" data-textcolor="#e73725"   data-date="2023-05-29 00:12" data-background="#e73725" data-digitscolor="#e73725" data-unitscolor="#e73725">Memorial Day Tournament in: </a>
   </section>
-  <section>
-
-    <table style="width:100%">
+  <div class="d-flex justify-content-center" >
+    <table class ="table table-bordered table-striped caption-top" style="width:100%">
       <caption>Top Teams:</caption>
       <tr>
         <th>Team</th>
@@ -49,9 +73,9 @@
         <td>6</td>
       </tr>
     </table>
-  </section>
-  <section>
-    <table>
+  </div>
+  <div class="d-flex justify-content-evenly">
+    <table class = "table table-bordered table-striped caption-top">
       <caption>Top Passers:</caption>
       <tr>
         <th>Name</th>
@@ -74,7 +98,7 @@
         <td>11</td>
       </tr>
     </table>
-    <table>
+    <table class = "table table-bordered table-striped caption-top">
       <caption>Top Rushers:</caption>
       <tr>
         <th>Name</th>
@@ -82,7 +106,7 @@
         <th>Rushing TD's</th>
       </tr>
       <tr>
-        <<td>Van Brookes</td>
+        <td>Van Brookes</td>
         <td>503</td>
         <td>10</td>
       </tr>
@@ -97,9 +121,9 @@
         <td>9</td>
       </tr>
     </table>
-  </section>
-  <section>
-    <table>
+  </div>
+  <div class="d-flex justify-content-evenly">
+    <table class = "table table-bordered table-striped caption-top ">
       <caption>Top Recievers:</caption>
       <tr>
         <th>Name</th>
@@ -122,7 +146,7 @@
         <td>12</td>
       </tr>
     </table>
-    <table>
+    <table class = "table table-bordered table-striped caption-top">
       <caption>Top Defenders:</caption>
       <tr>
         <th>Name</th>
@@ -145,6 +169,6 @@
         <td>12</td>
       </tr>
     </table>
-  </section>
+  </div>
 </body>
 </html>
